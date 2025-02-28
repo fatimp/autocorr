7feb1a286b98a922f661d876ef26d7dd341ac130
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-02-28 14:52:19 +0300

    Remove nsteps()

--- generation/generate.lua
+++ generation/generate.lua
@@ -1,5 +1,6 @@
 parameters = {
    {
+      ["%%STEPS%%"]   = 1,
       ["%%LENGTH%%"]  = 2,
       ["%%ILENGTH%%"] = 2,
       ["%%OMEGA%%"]   = 2,
@@ -7,6 +8,7 @@ parameters = {
       ["%%PRIME%%"]   = 3,
    },
    {
+      ["%%STEPS%%"]   = 2,
       ["%%LENGTH%%"]  = 4,
       ["%%ILENGTH%%"] = 4,
       ["%%OMEGA%%"]   = 2,
@@ -14,6 +16,7 @@ parameters = {
       ["%%PRIME%%"]   = 5,
    },
    {
+      ["%%STEPS%%"]   = 3,
       ["%%LENGTH%%"]  = 8,
       ["%%ILENGTH%%"] = 36,
       ["%%OMEGA%%"]   = 14,
@@ -21,6 +24,7 @@ parameters = {
       ["%%PRIME%%"]   = 41,
    },
    {
+      ["%%STEPS%%"]   = 4,
       ["%%LENGTH%%"]  = 16,
       ["%%ILENGTH%%"] = 16,
       ["%%OMEGA%%"]   = 3,
@@ -28,6 +32,7 @@ parameters = {
       ["%%PRIME%%"]   = 17,
    },
    {
+      ["%%STEPS%%"]   = 5,
       ["%%LENGTH%%"]  = 32,
       ["%%ILENGTH%%"] = 94,
       ["%%OMEGA%%"]   = 30,
@@ -35,6 +40,7 @@ parameters = {
       ["%%PRIME%%"]   = 97,
    },
    {
+      ["%%STEPS%%"]   = 6,
       ["%%LENGTH%%"]  = 64,
       ["%%ILENGTH%%"] = 190,
       ["%%OMEGA%%"]   = 87,
@@ -42,6 +48,7 @@ parameters = {
       ["%%PRIME%%"]   = 193,
    },
    {
+      ["%%STEPS%%"]   = 7,
       ["%%LENGTH%%"]  = 128,
       ["%%ILENGTH%%"] = 636,
       ["%%OMEGA%%"]   = 199,
@@ -49,6 +56,7 @@ parameters = {
       ["%%PRIME%%"]   = 641,
    },
    {
+      ["%%STEPS%%"]   = 8,
       ["%%LENGTH%%"]  = 256,
       ["%%ILENGTH%%"] = 256,
       ["%%OMEGA%%"]   = 74,
@@ -56,6 +64,7 @@ parameters = {
       ["%%PRIME%%"]   = 257,
    },
    {
+      ["%%STEPS%%"]   = 9,
       ["%%LENGTH%%"]  = 512,
       ["%%ILENGTH%%"] = 7666,
       ["%%OMEGA%%"]   = 1704,
@@ -63,6 +72,7 @@ parameters = {
       ["%%PRIME%%"]   = 7681,
    },
    {
+      ["%%STEPS%%"]   = 10,
       ["%%LENGTH%%"]  = 1024,
       ["%%ILENGTH%%"] = 13300,
       ["%%OMEGA%%"]   = 4085,
@@ -70,6 +80,7 @@ parameters = {
       ["%%PRIME%%"]   = 13313,
    },
    {
+      ["%%STEPS%%"]   = 11,
       ["%%LENGTH%%"]  = 2048,
       ["%%ILENGTH%%"] = 18424,
       ["%%OMEGA%%"]   = 5062,
@@ -77,6 +88,7 @@ parameters = {
       ["%%PRIME%%"]   = 18433,
    },
    {
+      ["%%STEPS%%"]   = 12,
       ["%%LENGTH%%"]  = 4096,
       ["%%ILENGTH%%"] = 12286,
       ["%%OMEGA%%"]   = 2015,
@@ -84,6 +96,7 @@ parameters = {
       ["%%PRIME%%"]   = 12289,
    },
    {
+      ["%%STEPS%%"]   = 13,
       ["%%LENGTH%%"]  = 8192,
       ["%%ILENGTH%%"] = 40956,
       ["%%OMEGA%%"]   = 2821,
@@ -91,6 +104,7 @@ parameters = {
       ["%%PRIME%%"]   = 40961,
    },
    {
+      ["%%STEPS%%"]   = 14,
       ["%%LENGTH%%"]  = 16384,
       ["%%ILENGTH%%"] = 114682,
       ["%%OMEGA%%"]   = 43,
@@ -98,6 +112,7 @@ parameters = {
       ["%%PRIME%%"]   = 114689,
    },
    {
+      ["%%STEPS%%"]   = 15,
       ["%%LENGTH%%"]  = 32768,
       ["%%ILENGTH%%"] = 163836,
       ["%%OMEGA%%"]   = 31285,
@@ -105,6 +120,7 @@ parameters = {
       ["%%PRIME%%"]   = 163841,
    },
    {
+      ["%%STEPS%%"]   = 16,
       ["%%LENGTH%%"]  = 65536,
       ["%%ILENGTH%%"] = 65536,
       ["%%OMEGA%%"]   = 7659,
@@ -112,6 +128,7 @@ parameters = {
       ["%%PRIME%%"]   = 65537,
    },
    {
+      ["%%STEPS%%"]   = 17,
       ["%%LENGTH%%"]  = 131072,
       ["%%ILENGTH%%"] = 1179640,
       ["%%OMEGA%%"]   = 93583,
@@ -120,13 +137,14 @@ parameters = {
    }
 }
 
+
 h = io.open("autocorr.c.in", "r")
 pattern = h:read("*all")
 h:close()
 
-for i, param in pairs (parameters) do
+for _, param in pairs (parameters) do
    code = string.gsub(pattern, "%%%%%u+%%%%", param)
-   name = "autocorr_" .. tostring(i) .. ".c"
+   name = "autocorr_" .. tostring(param["%%STEPS%%"]) .. ".c"
    h = io.open(name, "w")
    h:write (code)
    h:close()
