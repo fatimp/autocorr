d92c0073eb6c1eddd1296c92e751216d509023b6
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-04-10 07:18:57 +0300

    New data

---
+++ src/consts.h
@@ -0,0 +1,13 @@
+#ifndef __PARAM_INIT_H__
+#define __PARAM_INIT_H__
+
+#include <stdint.h>
+
+#define P 23068673
+#define MAX_STEPS 21
+
+extern uint64_t omegas[MAX_STEPS+1];
+extern uint64_t inv_omegas[MAX_STEPS+1];
+extern uint64_t inv_lengths[MAX_STEPS+1];
+
+#endif
