b66fd42d83aa562f965660874c566cb1f2002482
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-04-11 12:30:06 +0300

    Update benchmarks

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -1,37 +1,20 @@
-#if defined(__FreeBSD__)
-#include <sys/timespec.h>
-#endif
-#include <assert.h>
-#include <time.h>
-#include <stdio.h>
 #include <autocorr.h>
+#include "ubench.h"
 
-static double gettime ()
-{
-    struct timespec tp;
-    clock_gettime (CLOCK_MONOTONIC, &tp);
-    return (double)tp.tv_sec + (0.000000001 * (double)tp.tv_nsec);
-}
-
-int main () {
-#define N 30000
-#define M 5000
-    struct ac_buffers *buffers = ac_alloc (N);
-    assert (buffers != NULL);
-
-    uint64_t *src = ac_get_src (buffers);
-    for (int i = 0; i < N; i++) {
-        src[i] = random() & 1;
+#define BENCH(len) UBENCH_EX(fft, length_##len) {       \
+        struct ac_buffers *buffers = ac_alloc (len);    \
+        UBENCH_DO_BENCHMARK() {                         \
+            ac_autocorr(buffers);                       \
+        }                                               \
     }
 
-    double time = gettime ();
-    for (int i = 0; i < M; i++) {
-        ac_autocorr (buffers);
-    }
-    time = gettime() - time;
+BENCH (256)
+BENCH (2046)
+BENCH (32768)
+BENCH (65536)
+BENCH (131072)
+BENCH (262144)
+BENCH (524288)
+BENCH (1048576)
 
-    ac_free (buffers);
-    printf ("Length %i, time = %f\n", N, time / M);
-
-    return 0;
-}
+UBENCH_MAIN();
